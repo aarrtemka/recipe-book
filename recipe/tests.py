ffd4993f2d962c9e96de82e00547d814eccf73bc
aarrtemka <118116235+aarrtemka@users.noreply.github.com>
2024-05-31 16:30:01 +0300

    Update tests.py

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe
 
-# Create your tests here.
+class RecipeViewsTestCase(TestCase):
+    def setUp(self):
+        self.recipe = Recipe.objects.create(
+            name='Test Recipe',
+            description='Test Description',
+            ingredients='Test Ingredients',
+            instructions='Test Instructions',
+            date='2023-01-01'
+        )
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Recipe')
+
+    def test_recipe_detail_view(self):
+        response = self.client.get(reverse('recipe_detail', args=[self.recipe.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Recipe')
+        self.assertContains(response, 'Test Description')
